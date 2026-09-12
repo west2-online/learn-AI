b6278b863e84e845548b5f5e65d8fc8a8fe3d932
xhhy-star <ycx0630@163.com>
2026-09-12 22:42:04 +0800

    更新p10046.py代码，修复计数器注释问题

--- p10046.py
+++ p10046.py
@@ -1,7 +1,7 @@
 apples = list(map(int, input().split()))
 h = int(input())
 max_height = h + 30
-count = 0
+count = 0  #计数器，记录摘到的苹果数
 for height in apples:
     if height <= max_height:
         count += 1
